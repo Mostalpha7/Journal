72c7891bde6006a27d09902becc92033b8756a18
Mustapha Abdulmujeeb <mostalpha7@gmail.com>
2020-10-14 17:20:09 +0100

    a

---
+++ lib/src/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+          body: Center(child: Text('Home Page')),
+        ),
+        theme: ThemeData(
+          accentColor: Colors.pinkAccent,
+          primaryColor: Colors.black,
+        ));
+  }
+}
